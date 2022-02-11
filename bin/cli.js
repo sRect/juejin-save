766fedbd00ee72a2d922528b895f0938d20398b5
fangchaoqun <fang.chaoqun@nissei-china.cn>
2022-02-11 14:33:36 +0800

    feat: update

--- bin/cli.js
+++ bin/cli.js
@@ -2,74 +2,89 @@
 
 const inquirer = require("inquirer");
 const chalk = require("chalk");
+const ora = require("ora");
 const { Command } = require("commander");
+
 const program = new Command();
+const spinner = ora();
 
 // https://github.com/tj/commander.js/blob/HEAD/Readme_zh-CN.md#%E5%91%BD%E4%BB%A4
 program
   .version("0.1.0")
-  .command("create <project-name>")
-  .description("create a new project")
+  .command("create <resume-name>")
+  .description("create a resume")
   .action((name) => {
-    console.log(`project name: ${name}`);
     // https://github.com/SBoudrias/Inquirer.js/#objects
     inquirer
       .prompt([
         {
-          name: "project name",
-          message: `Confirm project ${name}?`,
+          name: "resume",
+          message: `Please confirm resume name: ${name}`,
           type: "confirm",
         },
         {
-          name: "input your name",
-          message: "input name",
+          name: "personName",
+          message: "Please input your name:",
           type: "input",
           validate: (val) => {
-            if (!val || !val.trim()) {
-              return chalk.red("The name cannot be empty, Please try again");
+            if (!val) {
+              console.log(chalk.yellow("For example: foo"));
+              return console.log(
+                chalk.red("× The name cannot be empty, Please try again")
+              );
             }
+
             return true;
           },
         },
         {
-          name: "features",
-          message: "Check the features needed for your project",
-          pageSize: 10,
+          name: "sex",
+          message: "Please picker your sex:",
+          type: "list",
+          choices: ["man", "woman"],
+        },
+        {
+          name: "hobby",
+          messge: "Please select your hobby(multiple):",
           type: "checkbox",
           choices: [
-            // 具体的选项
             {
-              name: "Babel",
-              value: "babel",
-              short: "Babel",
-              description:
-                "Transpile modern JavaScript to older versions (for compatibility)",
-              link: "https://babeljs.io/",
-              checked: true, // 默认选中
+              name: "music",
+              value: "music",
+              description: "music music",
+              checked: true,
             },
             {
-              name: "Router",
-              value: "router",
-              description: "Structure the app with dynamic pages",
-              link: "https://router.vuejs.org/",
-            },
-            {
-              name: "historyMode",
-              // 只有当上一个问题为true才显示这个问题
-              when: (answers) => answers.features.includes("router"),
-              type: "confirm",
-              message: `Use history mode for router? ${chalk.yellow(
-                `(Requires proper server setup for index fallback in production)`
-              )}`,
-              description: `By using the HTML5 History API, the URLs don't need the '#' character anymore.`,
-              link: "https://router.vuejs.org/guide/essentials/history-mode.html",
+              name: "code",
+              value: "code",
+              description: "code code",
             },
           ],
         },
       ])
       .then((answers) => {
-        // Use user feedback for... whatever!!
-        console.log("answers==>", answers);
+        console.log(chalk.green("✔ answers result:" + JSON.stringify(answers)));
+
+        spinner.color = "yellow";
+        spinner.start("start...");
+
+        setTimeout(() => {
+          // spinner.stop();
+
+          // spinner.succeed(chalk.green("生成成功"));
+
+          // spinner.fail(chalk.red("生成失败"));
+
+          spinner.stopAndPersist({
+            symbol: chalk.green("✔"),
+            text: chalk.green("generate success"),
+          });
+
+          console.log(`----${name}'s resume ----`);
+          console.log(chalk.green("name: "), answers.personName);
+          console.log(chalk.green("sex: "), answers.sex);
+          console.log(chalk.green("hobby: "), answers.hobby.join(", "));
+        }, 2000);
       })
       .catch((error) => {
         if (error.isTtyError) {
